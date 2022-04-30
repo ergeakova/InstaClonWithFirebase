2693a4bed306f239da4a0125bb9b5ed7162ef22f
ergeakova <ergeak@gmail.com>
2022-05-01 02:45:59 +0300

    Initial Commit

---
+++ InstaCloneAndFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneAndFirebase
+//
+//  Created by Erge Gevher Akova on 1.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
