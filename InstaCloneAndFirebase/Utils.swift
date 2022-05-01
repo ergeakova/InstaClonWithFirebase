94c84f3c880cb7f52562a96b83a860a7a811fea1
ergeakova <ergeak@gmail.com>
2022-05-01 06:31:39 +0300

    added utils and create user method

---
+++ InstaCloneAndFirebase/Utils.swift
@@ -0,0 +1,20 @@
+//
+//  Utils.swift
+//  InstaCloneAndFirebase
+//
+//  Created by Erge Gevher Akova on 1.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class Utils{
+    
+    func showBasicAlert(tit: String, msg: String) ->UIAlertController{
+        let alert = UIAlertController(title: tit, message: msg, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        return alert
+    }
+    
+}
--- InstaCloneAndFirebase/ViewController.swift
+++ InstaCloneAndFirebase/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     override var shouldAutorotate: Bool {return false}
+    var utl = Utils()
 
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
@@ -23,6 +25,17 @@ class ViewController: UIViewController {
     }
     
     @IBAction func singUpClicked(_ sender: Any) {
+        if (txtEmail.text != "" && txtPassword.text != "") {
+            Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { (authData, error) in
+                if error != nil {
+                    self.present(self.utl.showBasicAlert(tit: "Error", msg: error?.localizedDescription ?? "user could not be created"), animated: true, completion: nil)
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else{
+            self.present(utl.showBasicAlert(tit:"Error",msg:"email and password cannot be empty"), animated: true, completion: nil)
+        }
     }
 }
 
