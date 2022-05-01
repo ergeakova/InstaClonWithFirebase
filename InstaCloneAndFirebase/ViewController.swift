1409fb8f2b327d29a249ff4f824d29fbc24ab205
ergeakova <ergeak@gmail.com>
2022-05-01 04:55:14 +0300

    added firebase and login design

--- InstaCloneAndFirebase/ViewController.swift
+++ InstaCloneAndFirebase/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override var shouldAutorotate: Bool {return false}
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func singInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func singUpClicked(_ sender: Any) {
+    }
 }
 
